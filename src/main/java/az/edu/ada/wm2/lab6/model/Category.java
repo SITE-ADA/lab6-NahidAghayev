15f1dd7a41c280db76a816049a6fea05d82ef1fa
NahidAghayev <naghayev17893@ada.edu.az>
2026-04-01 22:11:02 +0400

    Add CategoryEntity and CategoryRepository

---
+++ src/main/java/az/edu/ada/wm2/lab6/model/Category.java
@@ -0,0 +1,25 @@
+package az.edu.ada.wm2.lab6.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
+}
